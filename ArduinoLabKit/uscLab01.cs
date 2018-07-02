6e305aa61ab358ac88205d94489c2979b976de2b
IDEAPAD320S\LenovoDEV <pasakorn.s@outlook.com>
2018-07-02 17:28:15 +0700

    add Communication class / add some controls

---
+++ ArduinoLabKit/uscLab01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoLabKit
+{
+    public partial class uscLab01 : UserControl
+    {
+        public uscLab01()
+        {
+            InitializeComponent();
+        }
+
+        private void uscLab01_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
