06a7c4f49db9f3244826a0c889360d75a7d8465e
IDEAPAD320S\LenovoDEV <pasakorn.s@outlook.com>
2018-07-03 09:10:29 +0700

    add Data class / add ControlPanel class

---
+++ ArduinoLabKit/ControlPanel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoLabKit
+{
+    public abstract class PanelContainer
+    {
+        private object _container ;
+    }
+
+    public abstract class PanelSource
+    {
+        private object _source;
+    }
+
+    public interface IAddSource 
+    {
+        void Add(PanelSource source, PanelContainer container);
+    }
+
+    public interface IDataManagemnet
+    {
+        void SetData(object data);
+        void GetData(object data);
+    }
+
+    class ControlPanel : IAddSource, IDataManagemnet
+    {
+        private object _container;
+        private object _source;
+
+        public object Container { get => _container; set => _container = value; }
+        public object Source { get => _source; set => _source = value; }
+
+        public void Add(PanelSource source, PanelContainer container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetData(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetData(object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArduinoLabKit/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoLabKit
+{
+    public abstract class Data<T>
+    {
+        private T _inputData;
+
+        public T InputData { get => _inputData; set => _inputData = value; }
+    }
+
+    public interface IDataTransform
+    {
+        object Transform(Data<object> data);
+    }
+
+    public class DataSender : IDataTransform
+    {
+        public object Transform(Data<object> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DataReader : IDataTransform
+    {
+        public object Transform(Data<object> data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
